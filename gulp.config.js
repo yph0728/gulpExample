0681c3cbaadd218626c787deeb5805b2fc0b673c
Eric Yu <yph0728@gmail.com>
2016-05-22 23:22:40 +1000

    css injection

--- gulp.config.js
+++ gulp.config.js
@@ -1,17 +1,40 @@
 module.exports = function () {
     var client = './src/client/';
+    var clientApp = client + 'app/';
+    var temp = './.tmp/';
     
     
     var config = {
-        temp : './.tmp/',
+        temp : temp,
         
         
       alljs:[
          './src/**/*.js',
          './*.js'      
       ],
-      
+      client : client,
+      css: temp + 'styles.css',
+      index: client + 'index.html',
+      js: [
+          clientApp + '**/*.module.js',
+          clientApp + '**/*.js',
+          '!' + clientApp + '**/*.spec.js'
+      ],
       less : client + 'styles/styles.less',
+      bower : {
+          json : require('./bower.json'),
+          directory: './bower_components/',
+          ignorePath : '../..'   
+      }
+    };
+    
+    config.getWiredepDefaultOptions = function () {
+        var options = {
+            bowerJson : config.bower.json,
+            directory : config.bower.directory,
+            ignorePath : config.bower.ignorePath
+        };
+        return options;
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -22,8 +22,8 @@ gulp.task('vet', function () {
         .pipe($.jshint.reporter('jshint-stylish', {verbose:true}))
         .pipe($.jshint.reporter('fail'));
 });
-
-gulp.task('styles',['clean-styles'], function () {
+//['clean-styles'],
+gulp.task('styles',  function () {
    log('Compiling less ----> css'); 
    
    return gulp
@@ -33,6 +33,27 @@ gulp.task('styles',['clean-styles'], function () {
        // .on('error', errorLogger)
         .pipe($.autoprefixer({ browsers:['last 2 version','> 5%' ] }))
         .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('wiredep', function () {
+    log('wire up the bower css js and our app js into html');
+    
+   var options = config.getWiredepDefaultOptions(); 
+   var wiredep = require('wiredep').stream;
+   return gulp
+   .src(config.index)
+   .pipe(wiredep(options))
+   .pipe($.inject(gulp.src(config.js)))
+   .pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject',['wiredep', 'styles'], function () {
+     log('wire up the app css  and our into html and call wiredep');
+    
+   return gulp
+   .src(config.index)
+   .pipe($.inject(gulp.src(config.css)))
+   .pipe(gulp.dest(config.client));
 });
 
 
